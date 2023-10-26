6a27dc4c984ca470dd48862df32678652015844e
ms-shakil <mdshakil8796@gmail.com>
2023-10-26 12:38:03 +0600

    basic dom

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello js")
+let val = document.getElementById("header")
+
+val.innerHTML ="Hello Bangladesh"
+
+let val2 = document.getElementsByClassName("lorem1")[0]
+
+val2.innerHTML = "about class name"
+// getElementsByTagName, getElementsByName and getElementByClassName those also working same bye index number
+// querySelector take all tag singaly, quearySelectorAll take all tag
+
+
+// onclick
+function clickMe(){
+    document.write("hei ki obsta!")
+}
+function myFun(){
+    document.getElementById("time_box").innerHTML = Date()
+}
