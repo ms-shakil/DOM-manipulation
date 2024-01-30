99ef2b322efd7bc3acd7457a1b85fac49c84a940
ms-shakil <mdshakil8796@gmail.com>
2024-01-31 01:04:21 +0600

    dom practice

---
+++ app.js
@@ -0,0 +1,10 @@
+let header = document.getElementById("main-header")
+console.log(header)
+
+let listItems = document.getElementsByClassName("list-item")
+console.log((listItems))
+let listItemByTag = document.getElementsByTagName("li")
+console.log(listItemByTag)
+
+
+document.querySelector("#main-header").innerHTML ="Favorute Sports Frachise"
