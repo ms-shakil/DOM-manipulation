7e0b0d3fd2e3ca8d59faeb7361496fc48822d079
ms-shakil <mdshakil8796@gmail.com>
2024-01-31 03:02:53 +0600

    about dom

--- app.js
+++ app.js
@@ -24,3 +24,36 @@ function MsFun(){
 function SamFun(){
     my_img.src = "img/samiya.jpg"
 }
+
+// creat tag by js
+let newElement = document.createElement("h5")
+let text = document.createTextNode("this text creat by js")
+newElement.appendChild(text)
+
+var mainElement = document.querySelector(".main_element")
+mainElement.appendChild(newElement)
+
+
+// img slider
+
+const img_arr =["img/shakil.jpg","img/samiya1.jpg","img/shakil2.jpg","img/samiya.jpg"]
+let imgTag= document.getElementById("img")
+let count =0
+function NextFun(){
+ count++
+ if( img_arr.length <=count){
+    count =0
+    imgTag.src = img_arr[count]
+ }
+ imgTag.src=img_arr[count]
+}
+
+
+function PrevFun(){
+    count--
+    if(  count <0){
+       count =img_arr.length -1
+       imgTag.src = img_arr[count]
+    }
+    imgTag.src=img_arr[count]
+   }
