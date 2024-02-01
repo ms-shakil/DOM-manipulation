b76f51899b8230616a201b7a26c51e58d06c95fe
ms-shakil <mdshakil8796@gmail.com>
2024-02-01 16:13:11 +0600

    dom practice

--- app.js
+++ app.js
@@ -77,3 +77,51 @@ learnJs.addEventListener("mouseover",function(){
 learnJs.addEventListener("mouseout",function(){
    learnJs.classList.remove("text_style")
 })
+
+// multi addeventlistern add
+
+let myBtn = document.querySelectorAll(".my_button")
+for(let i =0;i<3;i++){
+   myBtn[i].addEventListener("click",function(){
+      let text =this.innerHTML
+      document.querySelector("#text_area").innerHTML = text +"  is clicked"
+   })
+}
+
+// audio play
+
+for(let i =0;i<3;i++){
+   document.querySelectorAll(".audio_btn")[i].addEventListener("click",function(){
+      let btn_text = this.innerHTML
+      console.log(btn_text)
+      audioPlay(btn_text)
+
+   })
+}
+// key press
+
+for(let i =0;i<3;i++){
+   document.querySelectorAll(".audio_btn")[i].addEventListener("keypress",function(event){
+     var inner_text = event.key
+      audioPlay(inner_text)
+
+   })
+}
+
+function audioPlay(text){
+   switch(text){
+      case "A":
+         var mp =new Audio("soundss/a1.mp3")
+         mp.play()
+         break;
+      case "B":
+         var mp1 =new Audio("soundss/a2.mp3")
+         mp1.play()
+         break;
+
+      case "C":
+         var mp2 =new Audio("soundss/a3.mp3")
+         mp2.play()
+         break;   
+   }
+}
